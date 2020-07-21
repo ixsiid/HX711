f9f919dc5f183508b8457a417f72467f66131b81
unknown <tal@HA000102600.mcdiv.csg.kyocera.co.jp>
2020-07-21 10:01:15 +0900

    Noise verification

--- examples/m5stack-core-esp32/src/main.cpp
+++ examples/m5stack-core-esp32/src/main.cpp
@@ -8,6 +8,21 @@ extern "C" {
 void app_main();
 }
 
+
+void sprint_bin(char * buffer, int32_t value) {
+	char t[16];
+	sprintf(t, "%o", value);
+
+	size_t j = 0;
+	char oct[8][4] = {
+		"000", "001", "010", "011", "100", "101", "110", "111"
+	};
+	for(size_t i =0; t[i] != '\0'; i++) {
+		sprintf(buffer + j, "%s", oct[t[i] - '0']);
+		j += 3;
+	}
+}
+
 void app_main() {
 	LCD::ILI9341 *lcd = new LCD::ILI9341();
 
@@ -20,6 +35,21 @@ void app_main() {
 
 		int32_t s1 = 0, s2 = 0;
 
+		int32_t history[11];
+		size_t h = 0;
+
+		while(scale1->is_ready()) {
+			s1 = scale1->read();
+			for(int i=0; i<11; i++) {
+				history[i] = s1;
+			}
+		}
+
+		char lines[11][64];
+		char l[64];
+		for(int i=0; i<11; i++) lines[i][0] = '\0';
+
+
 		while (true) {
 			vTaskDelay(10);
 
@@ -28,14 +58,38 @@ void app_main() {
 				s1 = scale1->read();
 				sprintf(buf, "SCALE1: %d      ", s1);
 				lcd->drawString(20, 25, LCD::WHITE, buf);
+
+				history[h++] = s1;
+				if (h >= 11) h = 0;
+
+				int32_t sum = 0;
+				for(int i=0; i<11; i++) sum += history[i];
+				int32_t ave = sum / 11;
+				size_t t = h + 6;
+				while (t >= 11) t-=11;
+				sprintf(buf, "%d, %d (%d)", sum, ave, history[t]);
+				lcd->drawString(50, 3, LCD::WHITE, buf);
+				float diff = history[t] - ave;
+				float thr = ave * 0.2f;
+				diff *= diff;
+				thr *= thr;
+				if (diff > thr) {
+					for(int i=0; i<11; i++) {
+						size_t n = t + 6 + i;
+						while(n >= 11) n -= 11;
+						sprint_bin(l, history[n]);
+						sprintf(lines[n], "%33s %d", l, history[n]);
+					}
+				}
 			}
+			for(int i=0; i<11; i++) lcd->drawString(5, 70 + i * 15, LCD::WHITE, lines[i]);
+
 			if (scale2->is_ready()) {
 				s2 = scale2->read();
 				sprintf(buf, "SCALE2: %d      ", s2);
-				lcd->drawString(20, 50, LCD::WHITE, buf);
+				lcd->drawString(20, 40, LCD::WHITE, buf);
 			}
-			sprintf(buf, "SUM   : %d      ", s1 + s2);
-			lcd->drawString(20, 80, LCD::WHITE, buf);
+			
 			lcd->update();
 		}
 	}, "HX711", 4096, lcd, 1, NULL, 1);
